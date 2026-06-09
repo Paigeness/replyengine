54804016910dac82a4ad7184bf369bcf2af99740
ReplyEngine <replyengine@deploy.vercel.app>
2026-06-09 15:12:35 +0000

    Fix Separator component type for orientation prop

--- src/components/ui/separator.tsx
+++ src/components/ui/separator.tsx
@@ -1,9 +1,23 @@
 import * as React from "react"
 import { cn } from "@/lib/utils"
 
-const Separator = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(({ className, orientation, ...props }, ref) => (
-  <div ref={ref} className={cn("shrink-0 bg-border", orientation === "vertical" ? "h-full w-px" : "h-px w-full", className)} {...props} />
-))
+interface SeparatorProps extends React.HTMLAttributes<HTMLDivElement> {
+  orientation?: "horizontal" | "vertical"
+}
+
+const Separator = React.forwardRef<HTMLDivElement, SeparatorProps>(
+  ({ className, orientation = "horizontal", ...props }, ref) => (
+    <div
+      ref={ref}
+      className={cn(
+        "shrink-0 bg-border",
+        orientation === "vertical" ? "h-full w-px" : "h-px w-full",
+        className
+      )}
+      {...props}
+    />
+  )
+)
 Separator.displayName = "Separator"
 
 export { Separator }
