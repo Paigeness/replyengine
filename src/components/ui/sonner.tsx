afa45d46e34752d8d45f656928598b4e8b6d7276
Paigeness <paigehmattke@gmail.com>
2026-07-13 22:26:14 -0400

    Refactor Toaster component for simplicity
    Simplify Toaster component to use default Sonner settings.

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -1,16 +1,5 @@
 "use client"
-
 import { Toaster as Sonner } from "sonner"
-
 export function Toaster() {
-  return (
-    <Sonner
-      className="toaster group"
-      toastOptions={{
-        classNames: {
-          toast: "cn-toast",
-        },
-      }}
-    />
-  )
+  return <Sonner />
 }
